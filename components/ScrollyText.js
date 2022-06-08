3525894b8eabf2389a8b69c0775704b43970143f
unknown <tim.manankil@gmail.com>
2022-06-08 13:28:31 +0800

    test scrolltrigger registerPlugin out of function

--- components/ScrollyText.js
+++ components/ScrollyText.js
@@ -2,35 +2,33 @@ import styles from "../styles/ScrollyText.module.scss";
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { useEffect } from "react";
-
-
-
+gsap.registerPlugin(ScrollTrigger);
 
 const ScrollyText = ({ text }) => {
 
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
+    
     gsap.fromTo(
       ".scrollyContainer",
-      { x: -120 },
+      { xPercent: -120 },
       {
-        x: 0,
+        xPercent: 0,
         duration: 1.5,
         scrollTrigger: {
           trigger: "#gallerySectionSpacer",
-          start: "50% 50%",
+          start: "25% 80%",
           toggleActions: "play none none reverse",
-          markers: true,
+          // markers: true,
         },
       }
     );
 
-    // gsap.to(".scrollyWords", {
-    //   xPercent: -50,
-    //   ease: "none",
-    //   duration: 15,
-    //   repeat: -1,
-    // });
+    gsap.to(".scrollyWords", {
+      xPercent: -50,
+      ease: "none",
+      duration: 20,
+      repeat: -1,
+    });
 
     // ScrollTrigger.refresh();
   }, []);
